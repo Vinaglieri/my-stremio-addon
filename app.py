484d29d78300159504ac5a593ce04f74126804a6
Vincenzo Aglieri Rinella <vincenzoaglieririnella@MacBook-Air-di-Vincenzo.local>
2026-05-24 13:22:26 +0300

    add stremio endpoints

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify
 app = Flask(__name__)
-
-MANIFEST = {
-    "id": "vinaglieri.addon",
-    "version": "1.0.0",
-    "name": "vinaglieri addon",
-    "description": "Personal Stremio add-on",
-    "types": ["movie", "series"],
-    "resources": [
-        "catalog",
-        {"name": "meta", "types": ["series"], "idPrefixes": ["hpy"]},
-        {"name": "stream", "types": ["movie", "series"], "idPrefixes": ["tt", "hpy"]}
-    ],
-    "catalogs": [
-        {"type": "movie", "id": "vinaglieri movies"},
-        {"type": "series", "id": "vinaglieri series"}
-    ]
-}
-
-@app.get("/manifest.json")
-def manifest():
-    return jsonify(MANIFEST)
 
 @app.get("/")
 def root():
     return "ok"
+
+@app.get("/manifest.json")
+def manifest():
+    return jsonify({
+        "id": "vinaglieri.addon",
+        "version": "1.0.0",
+        "name": "vinaglieri addon",
+        "description": "Personal Stremio add-on",
+        "resources": ["stream"],
+        "types": ["movie", "series"],
+        "catalogs": []
+    })
+
+@app.get("/stream/<type>/<id>.json")
+def stream(type, id):
+    return jsonify({
+        "streams": [
+            {
+                "name": "Test stream",
+                "url": "https://example.com"
+            }
+        ]
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
